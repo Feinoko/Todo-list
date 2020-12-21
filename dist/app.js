befde38a8746b80a7c66ee72002f1c876dd2e257
feinoko <feinoko@gmail.com>
2020-12-21 09:21:50 +0100

    appearance update

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+/* features / functionality
+  primary:
+  adding tasks to the task list
+  saving added tasks to local storage
+  removing individual tasks
+  searching specific tasks by filtering them
+  clearing all tasks
+
+  secondary
+  add night mode
+*/
+
+/* coding guidelines memo 
+KISS : Keep It Stupid Simple
+DRY : Dont Repeat Yourself
+YAGNI : You Aint Gonna Need it
+Break it down to very small bits
+Organise structure beforehand
+*/
+
+/* EVENT LISTENERS */
+
+
+/* EVENT HANDLERS */
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+      lg: '0 10px 15px 2px rgba(0, 0, 0, 0.15), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    },
     extend: {
       transitionDuration: {
         '400': '400ms',
